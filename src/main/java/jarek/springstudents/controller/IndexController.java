84051356a854ca68e4f37cdee30c29d1cb8e5fe2
Jarekszu2 <jarekszu2@onet.pl>
2022-03-02 21:30:27 +0100

    Request param. Path variable.

--- src/main/java/jarek/springstudents/controller/IndexController.java
+++ src/main/java/jarek/springstudents/controller/IndexController.java
@@ -1,14 +1,34 @@
 package jarek.springstudents.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(path = "/demo/") // warto dodać drugi / (wtedy jak zapomnimy ponięj się nie wywala; wymuszamy tak dodatkową ścieżkę demo
 public class IndexController {
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String wyswietlMojaStroneHelloWorldTySpringu() {
         return "strona-Hallo";
+    }
+
+    @GetMapping(path = "/hallo")
+    public String wyswietlHallo() {
+        return "strona-Hallo";
+    }
+
+    @GetMapping(path = "/halloTo") // /halloTo?imie=Jarek (required=false - brak parametru nie wywala strony)
+    public String wyswietlHalloTo(Model model,
+                                  @RequestParam(name = "imie", required = false) String parametrImie) {
+        model.addAttribute("atrybutImie", parametrImie);
+        return "strona-Hallo-To";
+    }
+
+    @GetMapping(path = "/halloMyBaby/{babyName}") // halloMyBaby/Jarek
+    public String wyswietlHalloToBaby(Model model,
+                                      @PathVariable(name = "babyName") String variable) {
+        model.addAttribute("atrybutImie", variable);
+        return "strona-Hallo-To";
     }
 }
