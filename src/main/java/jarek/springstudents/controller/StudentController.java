f13a36ec62e54986b016de161bd39c2bfc7d2c2e
Jarekszu2 <jarekszu2@onet.pl>
2022-03-27 22:07:27 +0200

    Student CRUD.

--- src/main/java/jarek/springstudents/controller/StudentController.java
+++ src/main/java/jarek/springstudents/controller/StudentController.java
@@ -5,11 +5,10 @@ import jarek.springstudents.service.StudentService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -20,6 +19,7 @@ public class StudentController {
     @GetMapping(path = "/add")
     public String add(Model model, Student student) {
         model.addAttribute("student", student);
+
         return "student-add";
     }
 
@@ -30,10 +30,49 @@ public class StudentController {
         return "redirect:/student/list";
     }
 
+    @GetMapping(path = "/edit/{edited_student_id}")
+    public String edit(Model model,
+                       @PathVariable( name = "edited_student_id") Long studentId) {
+        return editStudent(model, studentId);
+    }
+
+    @GetMapping(path = "/edit")
+    public String editParam(Model model,
+                            @RequestParam(name = "edited_student_id") Long studentId) {
+        return editStudent(model, studentId);
+    }
+
+    private String editStudent(Model model, Long studentId) {
+        Optional<Student> studentOptional = studentService.findById(studentId);
+        if (studentOptional.isPresent()) {
+            model.addAttribute("student", studentOptional.get());
+            return "student-add";
+        }
+
+        return "redirect:/student/list";
+    }
+
     @GetMapping(path = "/list")
     public String list(Model model) {
         List<Student> studentList = studentService.getStudentlist();
         model.addAttribute("students", studentList);
+
         return "student-list";
+    }
+
+    @GetMapping(path = "/delete/{deleted_student_id}")
+    public String delete(@PathVariable(name = "deleted_student_id") Long studentId) {
+        return deleteStudent(studentId);
+    }
+
+    @GetMapping(path = "/delete")
+    public String deleteParam(@RequestParam(name = "deleted_id") Long studentId) {
+        return deleteStudent(studentId);
+    }
+
+    private String deleteStudent(Long studentId) {
+        studentService.deleteById(studentId);
+
+        return "redirect:/student/list";
     }
 }
--- src/main/java/jarek/springstudents/service/StudentService.java
+++ src/main/java/jarek/springstudents/service/StudentService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -18,5 +19,13 @@ public class StudentService {
 
     public void add(Student student) {
         studentRepository.save(student);
+    }
+
+    public Optional<Student> findById(Long studentId) {
+        return studentRepository.findById(studentId);
+    }
+
+    public void deleteById(Long studentId) {
+        studentRepository.deleteById(studentId);
     }
 }
